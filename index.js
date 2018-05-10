f84b39573c0ac8bf472a7eaac8c71c44fdc32d70
Sergey Agadzhanov <serge@imac.local>
2018-05-10 18:16:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	core: 		       require('./lib/core'),
+	different: 	       require('./lib/different'),
+	dataextension:     require('./lib/dataextension'),
+	subscriber:        require('./lib/subscriber'),
+	automation:        require('./lib/automation')
+}

9fdcda9438d1223190a146614e771ec4a07003de
Sergey Agadzhanov <serge@imac.local>
2019-08-13 22:23:05 +0200

    createContentBlock

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
-	core: 		       require('./lib/core'),
-	different: 	       require('./lib/different'),
-	dataextension:     require('./lib/dataextension'),
-	subscriber:        require('./lib/subscriber'),
-	automation:        require('./lib/automation')
+	core: require('./lib/core'),
+	different: require('./lib/different'),
+	dataextension: require('./lib/dataextension'),
+	subscriber: require('./lib/subscriber'),
+	automation: require('./lib/automation'),
+	contentbuilder: require('./lib/contentbuilder')
 }
